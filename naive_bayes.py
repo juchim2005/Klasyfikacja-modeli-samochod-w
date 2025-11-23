32cb145bf1dacb4e446ec5123fc990e005d041f8
juchim2005 <63352963+juchim2005@users.noreply.github.com>
2025-11-24 00:29:51 +0100

    naive_bayes with pandas

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,163 +1,62 @@
-import csv
-import random
-import math
+import pandas as pd
 
-def load_csv(filename):
-    lines = csv.reader(open(filename, "r"))
-    dataset = list(lines)
-    for i in range(len(dataset)):
-        dataset[i] = [str(x) for x in dataset[i]]
-    return dataset
+def klasyfikuj_auto(auto):
+    max_wynik = -1
+    najlepsza_klasa = None
 
-def split_dataset(dataset, split_ratio):
-    train_size = int(len(dataset) * split_ratio)
-    train_set = []
-    test_set = list(dataset)
-    while len(train_set) < train_size:
-        index = random.randrange(len(test_set))
-        train_set.append(test_set.pop(index))
-    return [train_set, test_set]
+    for klasa in p_klasy.index:
+        wynik = p_klasy[klasa]
 
-def separate_by_class(dataset):
-    separated = {}
-    for i in range(len(dataset)):
-        vector = dataset[i]
-        if (vector[-1] not in separated):
-            separated[vector[-1]] = []
-        separated[vector[-1]].append(vector)
-    return separated
+        for cecha in auto.index:
+            if cecha == 'class':
+                continue
+            wartosc_cechy = auto[cecha]
 
-def mean(numbers):
-    return sum(numbers)/float(len(numbers))
+            p_cechy = p_cechy_dla_klasy[klasa][cecha].get(wartosc_cechy,1e-6)
 
-def stdev(numbers):
-    avg = mean(numbers)
-    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
-    return math.sqrt(variance)
+            wynik = wynik*p_cechy
 
-def summarize(dataset):
-    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-    del summaries[-1]
-    return summaries
-
-def summarize_by_class(dataset):
-    separated = separate_by_class(dataset)
-    summaries = {}
-    for class_value, instances in separated.items():
-        summaries[class_value] = summarize(instances)
-    return summaries
-
-def calculate_probability(x, mean, stdev):
-    if stdev == 0:
-        if x == mean:
-            return 1
-        else:
-            return 0
-    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
-
-def calculate_class_probabilities(summaries, input_vector):
-    probabilities = {}
-    for class_value, class_summaries in summaries.items():
-        probabilities[class_value] = 1
-        for i in range(len(class_summaries)):
-            mean, stdev = class_summaries[i]
-            x = input_vector[i]
-            probabilities[class_value] *= calculate_probability(x, mean, stdev)
-    return probabilities
-
-def predict(summaries, input_vector):
-    probabilities = calculate_class_probabilities(summaries, input_vector)
-    best_label, best_prob = None, -1
-    for class_value, probability in probabilities.items():
-        if best_label is None or probability > best_prob:
-            best_prob = probability
-            best_label = class_value
-    return best_label
-
-def get_predictions(summaries, test_set):
-    predictions = []
-    for i in range(len(test_set)):
-        result = predict(summaries, test_set[i])
-        predictions.append(result)
-    return predictions
-
-def get_accuracy(test_set, predictions):
-    correct = 0
-    for i in range(len(test_set)):
-        if test_set[i][-1] == predictions[i]:
-            correct += 1
-    return (correct/float(len(test_set))) * 100.0
-
-def get_attribute_value_counts(dataset):
-    counts = {}
-    for vector in dataset:
-        for i, value in enumerate(vector[:-1]):
-            if i not in counts:
-                counts[i] = {}
-            if value not in counts[i]:
-                counts[i][value] = 0
-            counts[i][value] += 1
-    return counts
-
-def calculate_conditional_probabilities(dataset, class_summaries, attribute_value_counts):
-    separated = separate_by_class(dataset)
-    probabilities = {}
-    for class_value, instances in separated.items():
-        probabilities[class_value] = {}
-        for i in range(len(instances[0]) - 1):
-            probabilities[class_value][i] = {}
-            for value in attribute_value_counts[i]:
-                count = 0
-                for instance in instances:
-                    if instance[i] == value:
-                        count += 1
-                probabilities[class_value][i][value] = (count + 1) / (len(instances) + len(attribute_value_counts[i]))
-    return probabilities
-
-def calculate_class_probabilities_categorical(class_summaries, conditional_probabilities, input_vector):
-    probabilities = {}
-    total_count = sum([class_summaries[label] for label in class_summaries])
-    for class_value, class_count in class_summaries.items():
-        probabilities[class_value] = class_count / total_count
-        for i in range(len(input_vector) - 1):
-            value = input_vector[i]
-            if value in conditional_probabilities[class_value][i]:
-                probabilities[class_value] *= conditional_probabilities[class_value][i][value]
-            else:
-                probabilities[class_value] *= 1 / (class_count + len(conditional_probabilities[class_value][i]))
-    return probabilities
+            if wynik > max_wynik:
+                max_wynik = wynik
+                najlepsza_klasa = klasa
+    return najlepsza_klasa
 
 
-def predict_categorical(class_summaries, conditional_probabilities, input_vector):
-    probabilities = calculate_class_probabilities_categorical(class_summaries, conditional_probabilities, input_vector)
-    best_label, best_prob = None, -1
-    for class_value, probability in probabilities.items():
-        if best_label is None or probability > best_prob:
-            best_prob = probability
-            best_label = class_value
-    return best_label
+nazwy_kolumn = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
 
-def main():
-    filename = 'car_evaluation.data'
-    split_ratio = 0.8
-    dataset = load_csv(filename)
+df = pd.read_csv('car_evaluation.data', names=nazwy_kolumn)
 
-    training_set, test_set = split_dataset(dataset, split_ratio)
+df = df.sample(frac=1).reset_index(drop=True)
 
-    separated_by_class_training = separate_by_class(training_set)
-    class_summaries = {class_value: len(instances) for class_value, instances in separated_by_class_training.items()}
-    attribute_value_counts = get_attribute_value_counts(training_set)
+punkt_podzialu = int(len(df) * 0.8)
 
-    conditional_probabilities = calculate_conditional_probabilities(training_set, class_summaries, attribute_value_counts)
+train_df = df[:punkt_podzialu]
+test_df = df[punkt_podzialu:]
+p_klasy = train_df['class'].value_counts(normalize=True)
 
-    predictions = []
-    for row in test_set:
-        prediction = predict_categorical(class_summaries, conditional_probabilities, row)
-        predictions.append(prediction)
+p_cechy_dla_klasy = {}
 
-    accuracy = get_accuracy(test_set, predictions)
-    print(f'Accuracy: {accuracy}%')
+for klasa in p_klasy.index:
+    podzbior = train_df[train_df['class'] == klasa]
 
-if __name__ == '__main__':
-    main()
+    p_cechy_dla_klasy[klasa] = {}
+
+    for kolumna in train_df.columns[:-1]:
+        prawdopodobienstwa = podzbior[kolumna].value_counts(normalize=True)
+
+        p_cechy_dla_klasy[klasa][kolumna] = prawdopodobienstwa
+
+dobre_odpowiedzi = 0
+
+
+for i in range(len(test_df)):
+    auto = test_df.iloc[i]
+    
+    prawdziwa_klasa = auto['class']
+    
+    przewidziana_klasa = klasyfikuj_auto(auto)
+
+    if prawdziwa_klasa == przewidziana_klasa:
+        dobre_odpowiedzi += 1
+dokladnosc = dobre_odpowiedzi/len(test_df)*100
+print(f"{dokladnosc}%")
